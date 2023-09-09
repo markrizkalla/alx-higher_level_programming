e9a8ca23d5f5cec08fc131a2bc007a45265219cd
markrizkalla <mark.rizkalla1@gmail.com>
2023-09-09 00:07:17 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -54,8 +54,17 @@ def solve(board, queenN, row):
 
 
 if __name__ == "__main__":
-    queenN = int(input("Enter No of Queens"))
-    board = makeBoard(queenN)
-    solution = solve(board, queenN, 0)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    nQueen = int(sys.argv[1])
+    board = makeBoard(nQueen)
+    solution = solve(board, nQueen, 0)
     for sol in solution:
         print(sol)
