8e313385ae82e223e2822d30b09fc0037e15e33a
markrizkalla <mark.rizkalla1@gmail.com>
2023-09-12 23:19:51 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read file data and print them"""
+
+
+def read_file(filename=""):
+    """func to read and print data in file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
