5577926e25f263310c766f60cd597fc9b72536e0
markrizkalla <mark.rizkalla1@gmail.com>
2023-09-14 02:50:10 -0700

    doc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,22 @@ from base import Base
 
 class Rectangle(Base):
     """Rectangle object inherit from Base cls"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+
+        """ Initialize a new rectangle
+        Args:
+        width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0."""
+
         self.width = width
         self.height = height
         self.x = x
@@ -14,11 +29,13 @@ class Rectangle(Base):
 
     @staticmethod
     def validator(method, val):
+        """ check if type of val"""
         if type(val) is not int:
             raise TypeError(f"{method} must be an integer")
 
     @property
     def width(self):
+         """Set/get the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -30,6 +47,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
+         """Set/get the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -41,6 +59,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Set/get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
@@ -52,6 +71,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Set/get the y coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
