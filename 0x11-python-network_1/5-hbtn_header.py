1ecb1aa255258a179133d284c3a4d0427caf7a48
markrizkalla <mark.rizkalla1@gmail.com>
2023-12-02 19:44:26 -0800

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" script that takes in URL send resques to the URL"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
