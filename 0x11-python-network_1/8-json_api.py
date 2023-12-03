9f7e928277dd9d2d150a7416adfbcf913854a633
markrizkalla <mark.rizkalla1@gmail.com>
2023-12-02 20:09:12 -0800

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    q = = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {'q': q}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    try:
+        res = r.json()
+        if re == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
