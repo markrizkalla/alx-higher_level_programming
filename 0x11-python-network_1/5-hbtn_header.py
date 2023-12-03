731756caed71fe68be74fd75f4da80df9aa6ded3
markrizkalla <mark.rizkalla1@gmail.com>
2023-12-02 19:46:26 -0800

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" script that takes in URL send resques to the URL"""
+""" script that takes in URL
+send resques to the URL"""
 
 import sys
 import requests
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
