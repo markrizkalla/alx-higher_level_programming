47faae81150efed0a1a04089278c24e37abf581b
markrizkalla <mark.rizkalla1@gmail.com>
2023-12-02 19:37:01 -0800

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that fetches url using requests pack"""
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print(f'- type: {type(r.text)}')
+    print(f'- content: {r.text}')
